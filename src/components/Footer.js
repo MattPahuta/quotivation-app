bceaa5db5dd4ad4b45f888b3eec74158a8ce820a
Matt Pahuta <mhpahuta@gmail.com>
2024-03-13 18:18:07 -0700

    Add quote components, update rendering

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,9 @@
 import React from "react";
 
+const currentYear = new Date().getFullYear();
+
 const Footer = () => {
-  return <footer className='wrapper'>&copy; 2023 Quotivation</footer>;
+  return <footer className='wrapper'>&copy; {currentYear} Quotivation</footer>;
 };
 
 export default Footer;
