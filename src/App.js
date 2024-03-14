8f00f0134e20c6632931ff4af93d9b7ce502d14d
Matt Pahuta <mhpahuta@gmail.com>
2024-03-14 16:02:21 -0700

    Add quote category rendering, filter render by category functionality

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import { Loader } from "react-feather";
 import "./App.css";
 
 function App() {
-
   const [quotes, setQuotes] = useState([]);
   const [loading, setLoading] = useState(false);
-
+  const [category, setCategory] = useState("All");
   const quotesUrl =
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
+  const categories = ["All", "Leadership", "Empathy", "Motivation", "Learning", "Success", "Empowerment"];
 
   const fetchQuotes = async () => {
     try {
@@ -29,12 +29,24 @@ function App() {
     fetchQuotes();
   }, [])
 
-  console.log(quotes);
+  // console.log(quotes);
+  // handle selecting a quote category option from CategoryForm select input
+  function handleCategoryChange(event) {
+    setCategory(event.target.value);
+  }
+  // filter quotes according to value currently in state
+  const filteredQuotes = category === "All" ? quotes : quotes.filter(quote => quote.categories.includes(category));
 
   return (
     <div className='App'>
       <Header />
-      <main>{loading ? <Loader /> : <Quotes quotes={quotes} />}</main>
+      <main>
+        {loading ? (
+          <Loader />
+        ) : (
+          <Quotes quotes={filteredQuotes} categories={categories} category={category} handleCategoryChange={handleCategoryChange} />
+        )}
+      </main>
       <Footer />
     </div>
   );
